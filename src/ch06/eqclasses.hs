b7e1daa41815d098564ede70e195cb65acd38c60
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2016-02-06 16:48:47 +0100

    some basic stuff with a `Colour` type and the `BasicEq3` typeclass.

--- src/ch06/eqclasses.hs
+++ src/ch06/eqclasses.hs
@@ -2,3 +2,30 @@
 
 class BasicEq a where
     isEqual :: a -> a -> Bool
+
+instance BasicEq Bool where
+    isEqual True    True    = True
+    isEqual False   False   = True
+    isEqual _       _       = False
+
+class BasicEq2 a where
+    isEqual2 :: a -> a -> Bool
+    isNotEqual2 :: a -> a -> Bool
+
+class BasicEq3 a where
+    isEqual3 :: a -> a -> Bool
+    isEqual3 x y = not (isNotEqual3 x y)
+
+    isNotEqual3 :: a -> a -> Bool
+    isNotEqual3 x y = not (isEqual3 x y)
+
+data Colour =   Red
+                | Green
+                | Blue
+                | White
+
+instance BasicEq3 Colour where
+    isEqual3 Red    Red     = True
+    isEqual3 Green  Green   = True
+    isEqual3 Blue   Blue    = True
+    isEqual3 _      _       = False
