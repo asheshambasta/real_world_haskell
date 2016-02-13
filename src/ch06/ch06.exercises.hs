32038f943ed80f5f049f4f3a4735dc5a4162badf
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2016-02-13 14:54:12 +0100

    problem 2, this was interesting

--- src/ch06/ch06.exercises.hs
+++ src/ch06/ch06.exercises.hs
@@ -1,5 +1,14 @@
 --file: ch06/ch06.exercises.hs
 
 import Control.Arrow
+
+-- #1
 roundSecond ::  (a, Double) -> (a, Int)
 roundSecond = second round
+
+-- #2
+mkPair :: a -> b -> (a, b)
+mkPair = (,)
+
+mkTriple :: a -> b -> c -> (a, b, c)
+mkTriple = (,,)
