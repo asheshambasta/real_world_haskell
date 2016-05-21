1f0791eec66762446bd9afd5cc9a0072d3b9b430
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2016-05-21 17:39:46 +0200

    neater highestClose

--- src/ch08recap/HighestClose.hs
+++ src/ch08recap/HighestClose.hs
@@ -1,5 +1,22 @@
 import qualified Data.ByteString.Lazy as L
 import Data.List.Split(splitOn)
+import qualified Data.ByteString.Lazy.Char8 as LC
 
-main = readFile "prices.csv" >>=
+ugly = readFile "prices.csv" >>=
        \contents -> return  (maximum ((map ((\ l -> read (l !! 4) :: Float) . splitOn ",") . tail . lines) contents))
+
+closing = readPrice . (!!4) . LC.split ','
+readPrice :: LC.ByteString -> Maybe Int
+readPrice str =
+    case LC.readInt str of
+        Nothing     -> Nothing
+        Just (dollars, rest) ->
+            case LC.readInt (LC.tail rest) of
+                Nothing -> Nothing
+                Just(cents, more) -> Just (dollars * 100 + cents)
+
+highestClose = maximum . (Nothing:) . map closing . LC.lines
+
+highestCloseFrom path = do
+    contents <- LC.readFile path
+    print (highestClose contents)
