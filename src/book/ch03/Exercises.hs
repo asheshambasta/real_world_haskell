d75f565902a525c58c725a5c5da0d540cd26540b
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2017-11-30 19:11:55 +0100

    more exercises

--- src/book/ch03/Exercises.hs
+++ src/book/ch03/Exercises.hs
@@ -23,3 +23,12 @@ listPalin2 xs = xs ++ listRev2 xs
   where listRev2 (x:xs) = listRev2 xs ++ [x]
         listRev2 [] = []
 
+
+isListPalin xs = check1By1 xs (rev2 xs)
+  where rev2 (x:xs) = rev2 xs ++ [x]
+        rev2 []     = []
+        check1By1 (x1:x1s) (x2:x2s) = (x1 == x2) && check1By1 x1s x2s
+        check1By1 [] []             = True
+        check1By1 _ []              = False
+        check1By1 [] _              = False
+
