58bb925b5c0fdecc3310f7010451ec1a39c0b066
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2017-12-07 09:02:28 +0100

    splitwith -- problem, match case should stop at a case but doesn't

---
+++ src/book/ch04/Exercises.hs
@@ -0,0 +1,35 @@
+-- file book/ch04/Exercises.hs
+
+safeHead (x:xs) = Just x
+safeHead []     = Nothing
+
+safeTail (x:xs) = Just xs
+safeTail []     = Nothing
+
+safeLast (x:[]) = Just x
+safeLast []     = Nothing
+safeLast (x:xs) = safeLast xs
+
+safeInit (x1:x2:[]) = Just [x1]
+safeInit (x:xs) =
+  case safeInit xs of
+    Nothing -> Just [x]
+    Just remInit  -> Just (x : remInit)
+safeInit []     = Nothing
+
+isEven :: Int -> Bool
+isEven x = (mod x 2) == 0
+
+splitWith :: (t -> Bool) -> [t] -> [[t]]
+splitWith func xs =
+  let (pre, sub) = break notFunc xs
+  in pre : case sub of
+    []    -> []
+    x:[]  -> [[x]]
+    _     -> splitWith func sub
+  where notFunc x1 = not (func x1)
+
+first xs = case xs of
+  []    -> Nothing
+  x:[]  -> Just x
+  _     -> Nothing
